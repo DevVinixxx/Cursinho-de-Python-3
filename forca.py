7e3ca4f51cb15ec4cea937c8b30674610e9504d8
Vinicius Peres <vperes505@gmail.com>
2022-12-23 03:24:22 -0300

    atualizações de mensagens vitoria/derrota

--- forca.py
+++ forca.py
@@ -77,13 +77,37 @@ def inicializa_letras_acertadas(palavra):
     return ["_" for letra in palavra]
 
 def msg_vencendor(palavra_secreta):
-    print("Parabens você venceu!! a palavra era: ")
-    print(palavra_secreta)
+    print("Parabéns, você ganhou!")
+    print("       ___________      ")
+    print("      '._==_==_=_.'     ")
+    print("      .-\\:      /-.    ")
+    print("     | (|:.     |) |    ")
+    print("      '-|:.     |-'     ")
+    print("        \\::.    /      ")
+    print("         '::. .'        ")
+    print("           ) (          ")
+    print("         _.' '._        ")
+    print("        '-------'       ")
 
 def msg_perdedor(palavra_secreta):
-    print("Que pena!")
-    print("Você perdeu, tente novamente")
-    print("A fruta era: ", palavra_secreta)
+    print("Puxa, você foi enforcado!")
+    print("A palavra era {}".format(palavra_secreta))
+    print("    _______________         ")
+    print("   /               \       ")
+    print("  /                 \      ")
+    print("//                   \/\  ")
+    print("\|   XXXX     XXXX   | /   ")
+    print(" |   XXXX     XXXX   |/     ")
+    print(" |   XXX       XXX   |      ")
+    print(" |                   |      ")
+    print(" \__      XXX      __/     ")
+    print("   |\     XXX     /|       ")
+    print("   | |           | |        ")
+    print("   | I I I I I I I |        ")
+    print("   |  I I I I I I  |        ")
+    print("   \_             _/       ")
+    print("     \_         _/         ")
+    print("       \_______/           ")
 
 if(__name__ == "__main__"):
     jogar()
